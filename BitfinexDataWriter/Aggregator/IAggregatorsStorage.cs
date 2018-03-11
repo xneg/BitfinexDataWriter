459e78c649e9f889abb27726cdd9a0db52a34552
Xneg <xnegxneg@gmail.com>
2018-03-11 14:15:57 +0300

    Написал функционал RawBooks, но еще не проверял.

--- BitfinexDataWriter/Aggregator/IAggregatorsStorage.cs
+++ BitfinexDataWriter/Aggregator/IAggregatorsStorage.cs
@@ -11,20 +11,5 @@ namespace BitfinexDataWriter.Aggregator
         void OnChannelCreated(SubscribedResponse subscribedResponse);
 
         void OnDataReceive(JToken data);
-
-        ///// <summary>
-        ///// Создать агрегатор и добавить его в хранилище.
-        ///// </summary>
-        ///// <param name="aggregatorType">Тип агрегатора.</param>
-        ///// <param name="channelId">ID канала.</param>
-        ///// <param name="instrumentName">Имя инструмента.</param>
-        //void CreateAggregator(AggregatorType aggregatorType, int channelId, string instrumentName);
-
-        ///// <summary>
-        ///// Получить агрегатор по ID канала.
-        ///// </summary>
-        ///// <param name="channelId">ID канала.</param>
-        ///// <returns>Экземпляр агрегатора.</returns>
-        //IAggregator GetAggregator(int channelId);
     }
 }
--- BitfinexDataWriter/Aggregator/RawBookAggregator.cs
+++ BitfinexDataWriter/Aggregator/RawBookAggregator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using BitfinexDataWriter.DataWriter;
 using BitfinexDataWriter.Orders;
@@ -8,29 +9,59 @@ namespace BitfinexDataWriter.Aggregator
 {
     public class RawBookAggregator : IAggregator<RawBook>
     {
-        private IDataWriter _dataWriter;
-        private int _channelId;
-        private string _instrumentName;
+        private readonly IDataWriter _dataWriter;
+        private readonly int _channelId;
+        private readonly string _instrument;
+        private readonly Dictionary<ulong, Order> _asks = new Dictionary<ulong, Order>();
+        private readonly Dictionary<ulong, Order> _bids = new Dictionary<ulong, Order>();
+
+        private double _bestAsk;
+        private double _bestBid;
 
         public RawBookAggregator(IDataWriter dataWriter, int channelId, string instrumentName)
         {
             _dataWriter = dataWriter;
             _channelId = channelId;
-            _instrumentName = instrumentName;
+            _instrument = instrumentName;
         }
 
         public void GetBook(RawBook book)
         {
-            throw new NotImplementedException();
+            AddOrder(FromRawBook(book));
+            UpdateBestPrices();
         }
 
         public void GetSnapshot(RawBook[] books)
         {
             foreach (var order in books.Select(FromRawBook))
             {
-                var x = order;
-                //AddOrder(order);
+                AddOrder(order);
             }
+
+            UpdateBestPrices();
+        }
+
+        private (double Bid, double Ask) GetBestPrices()
+        {
+            var bestBid = _bids?.Values.Max(b => b.Price) ?? 0;
+            var bestAsk = _asks?.Values.Min(a => a.Price) ?? 0;
+            return (bestBid, bestAsk);
+        }
+
+        private void UpdateBestPrices()
+        {
+            var (bid, ask) = GetBestPrices();
+            if (bid != _bestBid || ask != _bestAsk)
+            {
+                _bestBid = bid;
+                _bestAsk = ask;
+                _dataWriter.Write(GetResultData());
+            }
+        }
+
+        private ResultData GetResultData()
+        {
+            return new ResultData(_instrument, DateTime.UtcNow, _bestBid, _bestAsk);
         }
 
         private Order FromRawBook(RawBook book)
@@ -46,6 +77,47 @@ namespace BitfinexDataWriter.Aggregator
             else
             {
                 return Order.ToDelete(priceType, book.Price);
+            }
+        }
+
+        private void AddOrder(Order order)
+        {
+            var key = order.OrderId;
+
+            switch (order.PriceType)
+            {
+                case PriceType.Ask:
+                    if (order.NeedDelete)
+                    {
+                        _asks.Remove(key);
+                    }
+                    else
+                    {
+                        _asks.TryGetValue(key, out var currentOrder);
+                        _asks[key] = currentOrder + order;
+                        if (_asks[key].Amount <= 0)
+                        {
+                            _asks.Remove(key);
+                        }
+                    }
+
+                    break;
+                case PriceType.Bid:
+                    if (order.NeedDelete)
+                    {
+                        _bids.Remove(key);
+                    }
+                    else
+                    {
+                        _bids.TryGetValue(key, out var currentOrder);
+                        _bids[key] = currentOrder + order;
+                        if (_bids[key].Amount <= 0)
+                        {
+                            _bids.Remove(key);
+                        }
+                    }
+
+                    break;
             }
         }
 
