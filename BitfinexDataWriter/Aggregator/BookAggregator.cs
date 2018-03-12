94554295a73eea465553f612d4bfa9f2da4504b8
Xneg <xnegxneg@gmail.com>
2018-03-12 22:06:03 +0300

    Оптимизация BookAggregator.

--- BitfinexDataWriter/Aggregator/BookAggregator.cs
+++ BitfinexDataWriter/Aggregator/BookAggregator.cs
@@ -12,8 +12,9 @@ namespace BitfinexDataWriter.Aggregator
     public class BookAggregator : BaseAggregator, IAggregator<Book>
     {
         private readonly int _channelId;
-        private readonly Dictionary<double, double> _asks = new Dictionary<double, double>();
-        private readonly Dictionary<double, double> _bids = new Dictionary<double, double>();
+
+        private readonly SortedDictionary<double, double> _asks = new SortedDictionary<double, double>();
+        private readonly SortedDictionary<double, double> _bids = new SortedDictionary<double, double>();
 
         public BookAggregator(IDataWriter dataWriter, int channelId, string instrument) 
             : base(dataWriter, instrument)
@@ -53,38 +54,41 @@ namespace BitfinexDataWriter.Aggregator
 
         private void AddOrder(Order order)
         {
-            var key = order.Price;
 
             switch (order.PriceType)
             {
                 case PriceType.Ask:
+                    var keyAsk = order.Price;
+
                     if (order.NeedDelete)
                     {
-                        _asks.Remove(key);
+                        _asks.Remove(keyAsk);
                     }
                     else
                     {
-                        _asks.TryGetValue(key, out var currentCount);
-                        _asks[key] = currentCount + order.Amount;
-                        if (_asks[key] <= 0)
+                        _asks.TryGetValue(keyAsk, out var currentCount);
+                        _asks[keyAsk] = currentCount + order.Amount;
+                        if (_asks[keyAsk] <= 0)
                         {
-                            _asks.Remove(key);
+                            _asks.Remove(keyAsk);
                         }
                     }
 
                     break;
                 case PriceType.Bid:
+                    var keyBid = -order.Price;
+
                     if (order.NeedDelete)
                     {
-                        _bids.Remove(key);
+                        _bids.Remove(keyBid);
                     }
                     else
                     {
-                        _bids.TryGetValue(key, out var currentCount);
-                        _bids[key] = currentCount + order.Amount;
-                        if (_bids[key] <= 0)
+                        _bids.TryGetValue(keyBid, out var currentCount);
+                        _bids[keyBid] = currentCount + order.Amount;
+                        if (_bids[keyBid] <= 0)
                         {
-                            _bids.Remove(key);
+                            _bids.Remove(keyBid);
                         }
                     }
 
@@ -94,11 +98,8 @@ namespace BitfinexDataWriter.Aggregator
 
         protected override (double Bid, double Ask) GetBestPrices()
         {
-            var bidsValues = _bids.Keys;
-            var bestBid = bidsValues.Any() ? bidsValues.Max() : 0;
-
-            var askValues = _asks.Keys;
-            var bestAsk = askValues.Any() ? askValues.Min() : 0;
+            var bestBid = -_bids.Keys.FirstOrDefault();
+            var bestAsk = _asks.Keys.FirstOrDefault();
 
             return (bestBid, bestAsk);
         }
